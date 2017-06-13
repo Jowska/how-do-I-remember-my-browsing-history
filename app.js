f4df8983191e5db40ea1fa4a2f3c17d3c9009bf8
johanna jaskowska <johanns.jaskowska@gmail.com>
2017-06-13 22:35:41 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,17 @@
+var Telegram = require('node-telegram-bot-api')
+var settings = require('settings.json')
+var bot = new Telegram (settings.telegramToken, {polling : true})
+var history = require('./browserHistory.json')['Browser History']
+
+bot.on('message', function (msg) {
+  console.log(msg)
+  bot.sendMessage(msg.chat.id, "Hallo?")
+})
+
+var sendHistory = function() {
+  var randomLink = history[Math.floor(history.length * Math.random())].url
+  bot.sendMessage(settings.chatId, randomLink)
+}
+
+sendHistory()
+setInterval(sendHistory, 1000 * 60 * 60)
